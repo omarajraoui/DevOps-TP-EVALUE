b3d1f9dd55d2b478ff2492ca6572a17410cc6a3f
Omar <omarajraoui5@gmail.com>
2023-10-05 09:41:40 +0000

    change

--- main.tf
+++ main.tf
@@ -18,3 +18,40 @@ resource "google_sql_user" "wordpress" {
    instance = "main-instance"
    password = "password123"
 }
+resource "google_cloud_run_service" "default" {
+name     = "serveur-wordpress"
+location = "us-central1"
+
+template {
+   spec {
+      containers {
+      image = "europe-southwest1-docker.pkg.dev/contactapp-af5a5/website-tools/custom-wordpress"
+      ports {
+        container_port = 80
+
+        }
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+}
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
+}
